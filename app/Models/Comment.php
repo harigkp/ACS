786b6de75228fa01525ee7519613def7d76dee2d
Hari <bk.harigkp@gmail.com>
2022-06-26 22:04:06 +0545

    Add files via upload

---
+++ app/Models/Comment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model 
+{
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'comment',
+		'created',
+		'updated_at'
+    ];
+
+}
---
+++ app/Models/Review.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model 
+{
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'review',
+		'created',
+		'updated_at'
+    ];
+
+}
